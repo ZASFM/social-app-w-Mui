91f44c93e16e8fbcf5669d2bf6eaa9e9078ab5c4
Mohammad Shafi <shafi.bahrami.2015@gmail.com>
2022-12-02 14:17:49 +0100

    components responsive

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -3,7 +3,7 @@ import { Box } from '@mui/material';
 
 const Feed=()=>{
    return (
-      <Box bgcolor="lightgreen" flex={4}>
+      <Box bgcolor="lightgreen" flex={4} p={2}>
          Feed
       </Box>
    )
