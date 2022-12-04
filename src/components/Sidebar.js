9666f10eb576150abe8354c0c26f6bb37ab2235e
Mohammad Shafi <shafi.bahrami.2015@gmail.com>
2022-12-04 17:52:50 +0100

    Sidebar $ Feed with content

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,56 @@
 import React from 'react';
 import { Box } from '@mui/material';
+import {List, ListItem,ListItemButton,ListItemIcon,ListItemText,Home,Settings,Group,Pages, ModeNight, Switch} from '@mui/icons-material'
 
 const SideBar=()=>{
    return (
+      <div>
       <Box bgcolor="skyblue" flex={1} p={2} sx={{display:{xs:'none', sm:'block'}}}>
          SideBar
       </Box>
+      <List>
+      <ListItem disablePadding>
+        <ListItemButton component="a" href='#home'>
+          <ListItemIcon>
+            <Home/>
+          </ListItemIcon>
+          <ListItemText primary="home" />
+        </ListItemButton>
+      </ListItem>
+      <ListItem disablePadding>
+        <ListItemButton component="a" href='#pages'>
+          <ListItemIcon>
+            <Pages/>
+          </ListItemIcon>
+          <ListItemText primary="pages" />
+        </ListItemButton>
+      </ListItem>
+      <ListItem disablePadding>
+        <ListItemButton component="a" href='#setting'>
+          <ListItemIcon>
+            <Settings/>
+          </ListItemIcon>
+          <ListItemText primary="setting" />
+        </ListItemButton>
+      </ListItem>
+      <ListItem disablePadding>
+        <ListItemButton component="a" href='#friends'>
+          <ListItemIcon>
+            <Group/>
+          </ListItemIcon>
+          <ListItemText primary="friends" />
+        </ListItemButton>
+      </ListItem>
+      <ListItem disablePadding>
+        <ListItemButton component="a" href='#home'>
+          <ListItemIcon>
+            <ModeNight/>
+          </ListItemIcon>
+          <Switch/>
+        </ListItemButton>
+      </ListItem>
+      </List>
+      </div>
    )
 }
 
