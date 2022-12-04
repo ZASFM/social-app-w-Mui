fb48f1944a0158fdaa1bc1d741a81dc1d6bf724d
Mohammad Shafi <shafi.bahrami.2015@gmail.com>
2022-12-04 19:16:14 +0100

    Darkmode and post complements

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React,{useState} from 'react';
 import SideBar from './components/Sidebar';
 import RightBar from './components/Rightbar';
 import Feed from './components/Feed';
 import Navbar from './components/Navbar';
-import {Box,Stack,ThemeProvider} from '@mui/material'
+import {Box,Stack,ThemeProvider,createTheme} from '@mui/material'
 import theme from './components/theme';
 import Add from './components/Add';
 
 const App=()=>{
+  const [mode,setMode]=useState('light');
+  const darkTheme=createTheme({
+    palette:{
+      mode:mode,
+    }
+  })
   return(
-    <ThemeProvider theme={theme}>
-    <Box>
+    <ThemeProvider theme={darkTheme}>
+    <Box bgColor={"background.default"} text={"text.primary"}>
       <Navbar/>
        <Stack direction="row" justifyContent="space-between" spacing={2}>
           <SideBar/>
--- src/components/Add.js
+++ src/components/Add.js
@@ -1,5 +1,18 @@
-import { Fab, Tooltip,AddIcon, Button, Modal, Box } from "@mui/material";
+import { Fab, Tooltip,AddIcon, Avatar, Modal, Box, styled, Typography,TextField,Stack,EmojiEmotions,Image,VideoCameraBack,PersonAdd, ButtonGroup,DateRange,Button } from "@mui/material";
 import React,{useState} from "react";
+
+const StyledModal=styled(Modal)(({theme})=>({
+   display:'flex',
+   alignItem:'center',
+   justifyContent:'center',
+}))
+
+const UserBox=styled(Box)(({theme})=>({
+   display:'flex',
+   alignItem:'center',
+   gap:'10px',
+   marginBottom:'25px',
+}))
 
 const Add=()=>{
    const [open,setOpen]=useState(false);
@@ -10,16 +23,50 @@ const Add=()=>{
                <AddIcon />
             </Fab>
          </Tooltip>
-         <Modal
+         <StyledModal
+
             open={open}
             onClose={e=>setOpen(false)}
             aria-labelledby="parent-modal-title"
             aria-describedby="parent-modal-description"
          >
-            <Box>
-               Hello
+            <Box width={400} height={280} bgcolor='white' p={3} borderRadius={5}>
+               <Typography variant="h4" color="grey" textAlign="center">
+                  Create your post
+                  <UserBox>
+                     <Avatar
+                        img="/images/s"
+                        sx={{width:30,height:30}}
+                     />
+                     <Typography>Mohammad Shafi</Typography>
+                  </UserBox>
+                  <TextField
+                     sx={{width:'100%'}}
+                     multiline
+                     rows={4}
+                     id="filled-hidden-label-normal"
+                     placeholder="Type here"
+                     variant="filled"
+                  />
+               </Typography>
+               <Stack direction="row" gap={1} mt={2} mb={3}>
+                  <EmojiEmotions color="primary"/>
+                  <Image color="secondary"/>
+                  <VideoCameraBack color="success"/>
+                  <PersonAdd color="error"/>
+               </Stack>
+               <ButtonGroup
+                  fullWidth
+                  variant="contained"
+                  
+               >
+                  <Button>Add</Button>
+                  <Button>
+                     <DateRange/>
+                  </Button>
+               </ButtonGroup>
             </Box>
-         </Modal>
+         </StyledModal>
       </div>
    )
 }
